29b020369479c74e12e073fe61102bf9cb632b2a
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-07-20 09:23:40 +0200

    pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,10 +4,10 @@ Simple helper function
 """
 from typing import Tuple
 
-def index_range(page:int, page_size:int)-> Tuple:
+
+def index_range(page: int, page_size: int) -> Tuple:
     """this function will return a tuple"""
-    start_index = (page -1 ) * page_size
+    start_index = (page - 1) * page_size
     end_index = page * page_size
     my_tuple = (start_index, end_index)
     return my_tuple
-

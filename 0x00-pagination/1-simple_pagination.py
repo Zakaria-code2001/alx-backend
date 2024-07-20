67306da091748c7f030bf6ebee7843b8408093a6
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-07-20 10:04:22 +0200

    task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -2,47 +2,42 @@
 """
 Simple helper function
 """
-from typing import Tuple
 import csv
-import math
-from typing import List
+from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple:
-    """this function will return a tuple"""
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple containing the start and end index for the pagination."""
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    my_tuple = (start_index, end_index)
-    return my_tuple
+    return start_index, end_index
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        try:
-            if page not in range(1, 11) or page_size not in range(1, 11):
-                return []
-            assert type(page) == int and type(page_size) == int, "Both page and page_size must be integers."
-            start_index, end_index = index_range(page, page_size)
-            page_data = self.__dataset[start_index:end_index]
-            return page_data
+        """Return the appropriate page of the dataset."""
+        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
 
-        except AssertionError as e:
-            print(e)
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]

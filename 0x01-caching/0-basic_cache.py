29bcc3fbc5b04b6110e9ac7332a14ff14185cf22
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-07-24 06:58:06 +0000

    execution

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -13,11 +13,17 @@ class BasicCache(BaseCaching):
     """
 
     def put(self, key, item):
+        """
+        put item
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        get item
+        """
         if key is None:
             return None
         return self.cache_data[key]
